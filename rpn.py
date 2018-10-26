8a074e4d4f091fad706fbcc588cdc0dd622ab829
Zachary Durgan <ztdurgan@umich.edu>
2018-10-26 15:04:41 -0400

    Add Subtract test

--- rpn.py
+++ rpn.py
@@ -11,8 +11,11 @@ def calculate(arg):
         except ValueError:
             val1 = stack.pop()
             val2 = stack.pop()
-            result = val1 + val2
-            
+            if token = '+':
+                result = val1 + val2
+            else if token = '-':
+                result = val1 - val2
+                
             stack.append(result)
 
     return stack[0]
