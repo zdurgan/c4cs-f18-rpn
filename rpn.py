d3f0c10f631977eb8228354089256ec717722d6c
Zachary Durgan <ztdurgan@umich.edu>
2018-11-07 16:59:50 -0500

    Test for exponentiation

--- rpn.py
+++ rpn.py
@@ -15,9 +15,7 @@ def calculate(arg):
                 result = val1 + val2
             elif token == '-':
                 result = val1 - val2
-            elif token == '^':
-                result = pow(val1, val2)
-            
+                
             stack.append(result)
 
     if len(stack) > 1:
